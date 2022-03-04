b8be6928f1365c7fa9585e2314449ba0bab0f434
fyofio <muhammadisrofiordi@gmail.com>
2022-03-05 02:22:46 +0700

    perbaikan sedikit di header

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -4,7 +4,7 @@ pages << {
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
   url: "https://www.walmart.com/browse/movies-tv-shows/4096?facet=new_releases:Last+90+Days",
   fetch_type: "browser",
-  headers : {
+  headers: {
     "accept" =>	"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
   }
 }
