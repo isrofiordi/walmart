59dbbf3b98ccdabea20c6d73910d5ffc0262b184
fyofio <muhammadisrofiordi@gmail.com>
2022-03-07 23:04:30 +0800

    Perbaikan header untuk mengatasi verifikasi walmart

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -11,9 +11,9 @@ products.each do |product|
         fetch_type: 'browser',
         vars: {},
         headers: {
-            "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
-            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            # "Accept-Encoding" => "gzip, deflate, br",
+            "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "sec-ch-ua" => "'Not A;Brand';v='99', 'Chromium';v='98', 'Google Chrome';v='98'"
             # "Accept-Language" => "en-US,en;q=0.5",
             # "Host" => "www.walmart.com",
             # "Sec-Fetch-Dest" => "document",
@@ -21,7 +21,7 @@ products.each do |product|
             # "Sec-Fetch-Site" => "none",
             # "Sec-Fetch-User" => "?1",
             # "Upgrade-Insecure-Requests" => "1"
-            }
+        }
     }
 end
 
@@ -37,9 +37,9 @@ if next_page_link[0]
         fetch_type: 'browser',
         vars: {},
         headers: {
-            "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
-            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            # "Accept-Encoding" => "gzip, deflate, br",
+            "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "sec-ch-ua" => "'Not A;Brand';v='99', 'Chromium';v='98', 'Google Chrome';v='98'"
             # "Accept-Language" => "en-US,en;q=0.5",
             # "Host" => "www.walmart.com",
             # "Sec-Fetch-Dest" => "document",
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,9 +2,9 @@ pages << {
   page_type: "listings",
   method: "GET",
   headers: {
-        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0",
-        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
-        # "Accept-Encoding" => "gzip, deflate, br",
+        "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+        "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "sec-ch-ua" => "'Not A;Brand';v='99', 'Chromium';v='98', 'Google Chrome';v='98'"
         # "Accept-Language" => "en-US,en;q=0.5",
         # "Host" => "www.walmart.com",
         # "Sec-Fetch-Dest" => "document",
