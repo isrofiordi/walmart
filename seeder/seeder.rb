059cc10e6aee84234c8e12b652890d423baa1383
fyofio <muhammadisrofiordi@gmail.com>
2022-03-08 23:29:45 +0800

    perbaikan driver

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -14,14 +14,17 @@ pages << {
         "width": 1366,
         "height": 768
         },
-  driver: {   pre_code: "await page.setViewport({
+  driver: {
+          code: "
+            await page.setViewport({
               width: 1920 + Math.floor(Math.random() * 100),
               height: 3000 + Math.floor(Math.random() * 100),
               deviceScaleFactor: 1,
               hasTouch: false,
               isLandscape: false,
-              isMobile: false);",
-              disable_adblocker: true,
+              isMobile: false
+            });",
+          "disable_adblocker": true
   },
   # screenshot: {
   #         "take_screenshot": true,
