d4cc0b7e402a3025044e419a167d598cbaaa38ce
fyofio <muhammadisrofiordi@gmail.com>
2022-03-09 23:11:50 +0800

    copy header riyan

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -13,13 +13,15 @@ products.each do |product|
         fetch_type: 'browser',
         vars: {},
         headers: {
-            "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "sec-ch-ua" => "'Not A;Brand';v='99', 'Chromium';v='98', 'Google Chrome';v='98'",
-            "Accept-Encoding" => "gzip, deflate, br",
-            "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
-            "Sec-Ch-Ua-Arch" => "x86",
-            "Sec-Ch-Ua-Platform" => "Windows"
+            "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+            "sec-ch-ua-mobile"=> "?0",
+            "sec-ch-ua-platform"=> '"Linux"',
+            "sec-fetch-dest"=> "document",
+            "sec-fetch-mode"=> "navigate",
+            "sec-fetch-site" => "same-origin",
+            "sec-fetch-user" => "?1",
+            "upgrade-insecure-requests" => "1"
         },
         display: {
                 "width": 1366,
@@ -52,13 +54,15 @@ if next_page_link[0]
         fetch_type: 'browser',
         vars: {},
         headers: {
-            "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
-            "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "sec-ch-ua" => "'Not A;Brand';v='99', 'Chromium';v='98', 'Google Chrome';v='98'",
-            "Accept-Encoding" => "gzip, deflate, br",
-            "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
-            "Sec-Ch-Ua-Arch" => "x86",
-            "Sec-Ch-Ua-Platform" => "Windows"
+            "accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
+            "sec-ch-ua-mobile"=> "?0",
+            "sec-ch-ua-platform"=> '"Linux"',
+            "sec-fetch-dest"=> "document",
+            "sec-fetch-mode"=> "navigate",
+            "sec-fetch-site" => "same-origin",
+            "sec-fetch-user" => "?1",
+            "upgrade-insecure-requests" => "1"
         },
         display: {
                 "width": 1366,
