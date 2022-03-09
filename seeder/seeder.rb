2c61eecd3f6da259a7f0f65a63686bd7b1e64c9b
fyofio <muhammadisrofiordi@gmail.com>
2022-03-09 23:00:39 +0800

    perbaikan quote

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -4,11 +4,11 @@ pages << {
   headers: {
         "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
         "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "sec-ch-ua" => "'Not A;Brand';v='99', 'Chromium';v='98', 'Google Chrome';v='98'",
+        "sec-ch-ua" => '"Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
         "Accept-Encoding" => "gzip, deflate, br",
-        "Accept-Language" => "en-US,en;q=0.9,id;q=0.8",
-        "Sec-Ch-Ua-Arch" => "x86",
-        "Sec-Ch-Ua-Platform" => "Windows"
+        "Accept-Language" => 'en-US,en;q=0.9,id;q=0.8',
+        "Sec-Ch-Ua-Arch" => 'x86',
+        "Sec-Ch-Ua-Platform" => 'Windows'
     },
   display: {
         "width": 1366,
